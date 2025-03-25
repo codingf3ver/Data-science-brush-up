0ca1d7b1eb9ba684b849455c9e760256f822cf85
codingf3ver <official.tausif.786@gmail.com>
2025-03-25 11:46:58 +0530

    aws sagemaker pratice progressed

---
+++ aws/train.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import joblib
+from sklearn.linear_model import LinearRegression
+import os
+
+if __name__ == "__main__":
+    input_data_path = "/opt/ml/input/data/train/diabetes.csv"
+    data = pd.read_csv(input_data_path)
+    X = data.drop(columns=['target'])
+    y = data['target']
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    model_dir = os.environ["SM_MODEL_DIR"]
+    joblib.dump(model, f"{model_dir}/model.joblib")
